ae782480447230fdbfd9e5a83bc72a44b78668a5
Nelson <anthony69snake@gmail.com>
2020-08-30 11:53:13 -0600

    Iniciamos un request despues del otro (EN SERIE), y nos respondera el request en el orden de envio

--- class28.js
+++ class28.js
@@ -19,12 +19,34 @@ const API_URL_POKEMON = 'https://pokeapi.co/api/v2/'
 const POKEMON_URL = 'pokemon/:id'
 const opciones = { crossDomain: true }
 
-const RecibimosPokemon = (pokemon) => {
-    console.log(`Hola soy tu nuevo compañero y mi nombre es: ${pokemon.forms[0].name}, soy un pokemon de tipo ${pokemon.types[0].type.name}`)
-}
-const Obtener_POKEMOM = (id) => {
+
+// const Obtener_POKEMOM = (id,) => {
+//     const url = `${API_URL_POKEMON}${POKEMON_URL.replace(':id', id)}`
+//     $.get(url, opciones, RecibimosPokemon)
+// }
+
+
+//interaremos recibir en orden
+const Obtener_POKEMOM = (id, callback) => {
     const url = `${API_URL_POKEMON}${POKEMON_URL.replace(':id', id)}`
-    $.get(url, opciones, RecibimosPokemon)
+    $.get(url, opciones, (pokemon) => {
+        console.log(`Hola soy tu nuevo compañero y mi nombre es: ${pokemon.forms[0].name}, soy un pokemon de tipo ${pokemon.types[0].type.name}`)
+    })
+
+    if (callback) {
+        callback()
+    }
 }
 
-Obtener_POKEMOM(100);
+// es importante que aqui hagmos la referencia  al funcion
+// hacemos el request en serie
+Obtener_POKEMOM(1, function () {
+    Obtener_POKEMOM(2, function () {
+        Obtener_POKEMOM(3, function () {
+            Obtener_POKEMOM(4, function () {
+                Obtener_POKEMOM(5)
+            })
+        });
+    });
+});
+
